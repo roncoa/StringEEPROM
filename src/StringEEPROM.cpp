429b02aa4c0f2f796bb7c0ded2ebd2f313791c08
Roncoa <roncoa@gmail.com>
2025-02-06 12:12:14 +0100

    Update StringEEPROM.cpp

--- src/StringEEPROM.cpp
+++ src/StringEEPROM.cpp
@@ -1,3 +1,10 @@
+/**************************
+   StringEEPROM.cpp
+   v 1.0.0
+   by roncoa@gmail.com
+   06/02/2025
+ **************************/
+
 #include "StringEEPROM.h"
 #include <EEPROM.h>
 
